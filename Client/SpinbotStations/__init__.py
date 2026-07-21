3d35cab9900724c1f5a0660ec78f427d06883131
Addison Blackwelder <addisonblackwelder431@gmail.com>
2026-07-21 12:34:52 -0400

    Fixed up some image station code

--- Client/SpinbotStations/__init__.py
+++ Client/SpinbotStations/__init__.py
@@ -1,4 +1,5 @@
 from .image_station import imagestation
 from .sdc import sdc
+from .utils import parse_yaml, sort_steps
 
 __all__ = ['imagestation', 'sdc']
--- Client/script.py
+++ Client/script.py
@@ -1,6 +1,6 @@
 import os
+import SpinbotStations as SBS
 from dotenv import load_dotenv
-from SpinbotStations import imagestation
 from elnclient import ElnClient
 
 load_dotenv()
@@ -9,21 +9,21 @@ http_key = os.environ.get("HTTP_KEY")
 if eln_key == None or http_key == None:
     raise ValueError("Missing eln and/or http key")
 
-img_station = imagestation(
+img_station = SBS.imagestation(
     pi_url='http://100.107.255.14:8080',
     api_key=http_key 
     )
 
-# img_station.capture(filename='substrate_cap')
+img_station.capture(filename='substrate_cap')
 
-experiment = ElnClient(
-    url="https://eln.ddomlab.org/api/v2",
-    api_key=eln_key,
-    title="Spinbot capture test",
-    desc="This is a test of the API I am working with"
-    )
+# experiment = ElnClient(
+#     url="https://eln.ddomlab.org/api/v2",
+#     api_key=eln_key,
+#     title="Spinbot capture test",
+#     desc="This is a test of the API I am working with"
+#     )
 
-experiment.upload_file(
-    file_path=img_station.capture(filename='substrate_cap'), 
-    comment="Capture of substrate"
-    )
+# experiment.upload_file(
+#     file_path=img_station.capture(filename='substrate_cap'), 
+#     comment="Capture of substrate"
+#     )
